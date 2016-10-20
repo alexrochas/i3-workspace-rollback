a31a740a8d640a8157b185a8de4357dfa0de54fd
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-20 16:02:48 -0200

    feat: change lib to i3ipc

--- rollback.py
+++ rollback.py
@@ -1,19 +1,41 @@
-import i3
-import pickle
+import i3ipc
+
+# Create the Connection object that can be used to send commands and subscribe
+# to events.
+i3 = i3ipc.Connection()
+workspace_stack = []
 
 
-def persist_to_file(content, filename='./mock/event/state.data'):
-    with open(filename, 'wb') as f:
-        pickle.dump(content, f)
+# Define a callback to be called when you switch workspaces.
+def on_workspace_focus(i3, e):
+    if len(workspace_stack) == 0:
+        workspace_stack.append(e.old)
+        workspace_stack.append(e.current)
+    if e.current and e.old:
+        if e.current.name == e.old.name:
+            return
+        elif e.current.name == workspace_stack[-1].name:
+            return
+        else:
+            workspace_stack.append(e.current)
+    print([wspace.name for wspace in workspace_stack])
 
 
-def read_from_file(filename='./mock/event/state.data'):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
+def on_keys(i3, e):
+    if e.binding.symbol == 'z' and 'Mod4' in e.binding.mods and workspace_stack:
+        print([wspace.name for wspace in workspace_stack])
+        if workspace_stack and len(workspace_stack) == 1:
+            i3.command('workspace %s' % workspace_stack[0].name)
+            return
+        elif workspace_stack:
+            workspace_stack.pop()  # FIXME workaround
+            print('Rollback to workspace %s' % workspace_stack[-1].name)
+            i3.command('workspace %s' % workspace_stack.pop().name)
 
 
-def rollback_callback(event, data, subscription):
-    pass
+# Subscribe to events
+i3.on('workspace::focus', on_workspace_focus)
+i3.on('binding', on_keys)
 
-
-subscription = i3.Subscription(rollback_callback, 'workspace')
+# Start the main loop and wait for events to come in.
+i3.main()
